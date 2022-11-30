67cf51171bf676459a32c50043b33960afdfaa65
nadezda.matvejeva <nadezda.matvejeva@tripledev.ee>
2022-11-30 19:17:57 +0200

    conf fixed

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,7 @@ module.exports = defineConfig({
       baseUrl: 'https://jira.ivorreic.com/',
     },
     defaultCommandTimeout: 30000,
+    requestTimeout: 15000,
     projectId: "dpb32o",
   },
 
